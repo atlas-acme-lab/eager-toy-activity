ba88bd8abaf3701c7d8b4058593c4785600afe7b
Peter Gyory <ptgyory@gmail.com>
2021-04-01 15:19:55 -0600

    Update html to include bars

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # eager-toy-activity
+
+# Setup and Run
+You should just be able to open the html file in a browser and you are good to go!
+
+# What's already here
+I set up the structure for the project with a basic bar chart. More bars can be added fairly easily, you just need to create a new instance of Chart bar with the correct id. This class handles putting values into the fields on the left and connects them to a bar (need to add validation shown in the project.). I've also implemented a system which could easily be expanded upon to track the marker placement and map it to an integer between 0 and 10.
+
+# Notes!
+Currently I have it set up to only do bar chart functionality for debugging. At the bottom of the js file just swap the init function.
+
+The markers and other bars still need to be tied together, that wouldn't take me long (like 30 min) once I know everything we need so just gimme those deets
+
+IMPORTANT: Eventually we will need to serve this from HTTPS since webcam is only available from secure sites.
--- index.js
+++ index.js
@@ -18,40 +18,77 @@ const getColData = (marker, axisVector, axisLength) => {
   return (magnitude(x, y) / axisLength);
 }
 
+// Class for updating the bars. Provide the color component of the id in html
+class ChartBar {
+  constructor(id) {
+    this.input = document.querySelector(`#${id}-input`);
+    this.expected = document.querySelector(`#${id}-expected`);
+    this.set = document.querySelector(`#${id}-user-set`);
+
+    this.input.addEventListener('change', (e) => {
+      this.expected.style.height = `calc(${e.target.value * 10}% + ${window.innerHeight / 105}px)`;
+    });
+  }
+
+  // Sets bar to match value passed in
+  // TODO: add validation
+  setUserVal(v) {
+    this.set.style.height = `${v * 10}%`;
+  }
+}
+
+const bars = [];
+function initChart() {
+  // This just controls the chart
+  bars.push(new ChartBar('red'));
+} 
+
 function init() {
+  initChart();
   Beholder.init('#beholder-div');
 
+  // We still need anchors to track the bar chart
   bottomAnchor = Beholder.getMarker(0);
   topAnchor = Beholder.getMarker(1);
 
   // Do this for all the values you want
   columnData.push({
-    marker: Beholder.getMarker(2),
+    marker: Beholder.getMarker(2), // Get that marker ref
     value: 0,
     rawValue: 0,
+    chartID: 0, // This maps to the placement of the chart in the array
   });
 
   update();
 }
 
+// For tracking delta time
 let prevTime = Date.now();
 function update() {
   let currentTime = Date.now();
   let dt = currentTime - prevTime;
   prevTime = currentTime;
+
   Beholder.update();
 
+  // These vectors are for mapping the marker values to ints between 0 and 10
   const axisVec = topAnchor.center.clone().sub(bottomAnchor.center);
   const axisMag = axisVec.mag();
 
   // Update the column marker values
   columnData.forEach(cd => {
-    // should be btw zero and 1
+    // raw value should be btw zero and 1
     cd.rawValue = getColData(cd.marker, axisVec, axisMag);
     cd.value = Math.round(cd.rawValue * AXIS_STEPS);
+    bars[cd.chartID].set(cd.value);
   });
+
 
   requestAnimationFrame(update);
 }
 
-window.onload = init;
+// This is for just messing with the charts without the marker stuffs
+window.onload = initChart;
+
+// This is the real one
+// window.onload = init;
